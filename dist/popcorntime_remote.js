f65c447269607d09daa5fcf2673405cd61a27318
Giacomo Cerquone <cerquone@gmail.com>
2015-08-19 02:23:02 +0200

    added methods, fn with optional params and callback

--- dist/popcorntime_remote.js
+++ dist/popcorntime_remote.js
@@ -125,21 +125,35 @@ popcorntime_remote = {
     getsorters:         function(callback) { lib.APIcall("getsorters", false, callback); },
     gettypes:           function(callback) { lib.APIcall("gettypes", false, callback); },
     getcurrenttab:      function(callback) { lib.APIcall("getcurrenttab", false, callback); },
+    getfullscreen:      function(callback) { lib.APIcall("getfullscreen", false, callback); },
+    getplayers:         function(callback) { lib.APIcall("getplayers", false, callback); },
+    startstream:        function(params) { lib.APIcall("startstream", params); },
+    setplayer:          function(params) { lib.APIcall("setplayer", params); },
+    
 
     //Tv Show Detail
     nextseason:         function() { lib.APIcall("nextseason"); },
     previousseason:     function() { lib.APIcall("previousseason"); },
+    selectepisode:      function(params) { lib.APIcall("selectepisode", params); },
 
     //Movie Detail
     togglequality:      function() { lib.APIcall("togglequality"); },
     watchtrailer:       function() { lib.APIcall("watchtrailer"); },
+    getselection:       function(params, callback) { if (typeof params === "function") { callback = params; params = false; } lib.APIcall("getselection", params, callback); },
+    getcurrentlist:     function(params, callback) { if (typeof params === "function") { callback = params; params = false; } lib.APIcall("getcurrentlist", params, callback); },
+    getsubtitles:       function(callback) { lib.APIcall("getsubtitles", false, callback); },
 
     //Player
     togglemute:         function() { lib.APIcall("togglemute"); },
     togglefullscreen:   function() { lib.APIcall("togglefullscreen"); },
-    setvolume:          function(params) { lib.APIcall("volume", params); },
     toggleplaying:      function() { lib.APIcall("toggleplaying"); },
+    setvolume:          function(params, callback) { if (typeof params === "function") { callback = params; params = false; } lib.APIcall("volume", params, callback); },
     getplaying:         function(callback) { lib.APIcall("getplaying", false, callback) },
+    seek:               function(params) { lib.APIcall("seek", params); },
+    subtitleoffset:     function(params) { lib.APIcall("subtitleoffset", params); },
+    setsubtitle:        function(params) { lib.APIcall("setsubtitle", params); },
+    getstreamurl:       function(callback) { lib.APIcall("getstreamurl", false, callback) },
+    
 
     //Main Browser
     toggletab:          function() { lib.APIcall("toggletab"); },
@@ -161,6 +175,7 @@ popcorntime_remote = {
     filtersorter:       function(params) { lib.APIcall("filtersorter", params); },
     filtertype:         function(params) { lib.APIcall("filtertype", params); },
     filtersearch:       function(params) { lib.APIcall("filtersearch", params); },
+    setselection:       function(params) { lib.APIcall("setselection", params); },
 
     up:                 function() { lib.APIcall("up"); },
     down:               function() { lib.APIcall("down"); },
