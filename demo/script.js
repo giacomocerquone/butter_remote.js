be98c8ae3b1d396eabb823bff36176b4e743d193
Giacomo Cerquone <cerquone@gmail.com>
2015-09-09 02:34:03 +0200

    Updated demo to look like the ui of the app

--- demo/script.js
+++ demo/script.js
@@ -13,6 +13,7 @@ $(document).ready( function() {
         pr.init({ ip:ipVal, port: portVal, username: usernameVal, password: passwordVal }, function(data) {
             if(data.result) {
                 $("#login").hide();
+                $("#header").show();
                 $("#controller").show();
             } else {
                 alert("Couldn't connect. Check for username and password.");
@@ -41,11 +42,21 @@ $(document).ready( function() {
             })
         }, 1000);
 
+        pr.getcurrentlist(function(data) {
+            $.each(data.result.list, function(index, item) {
+                var html = '<li><a class="'+index+'"><img src="'+this.cover+'" width="134" /><p>'+this.title+'</p><p style="color:#5b5b5b; font-size:0.75em;">'+this.year+'</p></a></li>';
+                $("#movies > ul").append(html);
+            });
+            console.log(data.result.list);
+        });
+
     });
 
-
-
-
+    $(document).on("click", "#movies > ul > li > a", function() {
+        var toSelect = $(this).attr('class');
+        pr.setselection(toSelect);
+        pr.enter();
+    });
 
 
 });
