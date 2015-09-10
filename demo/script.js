ef0e53a5d1c46564b1dc21c85e671218405eaabe
Giacomo Cerquone <cerquone@gmail.com>
2015-09-10 12:06:53 +0200

    updated demo

--- demo/script.js
+++ demo/script.js
@@ -3,6 +3,28 @@ $(document).ready( function() {
     var pr = popcorntime_remote,
         connected = false;
 
+    function getlist(tab) {
+        $("#list > ul").empty();
+        if(tab == "movies") {
+            pr.getcurrentlist(function(data) {
+                console.log(data);
+                $.each(data.result.list, function(index, item) {
+                    var html = '<li><a class="'+index+' open-item"><img src="'+this.cover+'" width="134" /><p>'+this.title+'</p><p style="color:#5b5b5b; font-size:0.75em;">'+this.year+'</p></a></li>';
+                    $("#list > ul").append(html);
+                });
+            });
+        } else if(tab == "shows") {
+            pr.getcurrentlist(function(data) {
+                $.each(data.result.list, function(index, item) {
+                    var html = '<li><a class="'+index+' open-item"><img src="'+this.images['poster']+'" width="134" /><p>'+this.title+'</p><p style="color:#5b5b5b; font-size:0.75em;">'+this.year+'</p></a></li>';
+                    $("#list > ul").append(html);
+                });
+            });
+        } else if(tab == "anime") {
+
+        }
+    }
+
     $("#submit").click(function() {
 
         var ipVal = ( !$("#ip-address").val() ? "localhost" : $("#ip-address").val() );
@@ -27,36 +49,55 @@ $(document).ready( function() {
                     view = data.result.events.viewstack[data.result.events.viewstack.length-1];
                 }
 
-                console.log(data);
-
                 if(view == "main-browser") {
-                    $('.movie').hide();
-                    $('.main').show();
-                    $('.main').html("main-browser")
+                   
                 } else if(view == "movie-detail") {
-                    $('.main').hide();
-                    $('.movie').show();
-                    $('.movie').html("movie-detail")
                 }
+
+                //pr.getcurrenttab();
 
             })
         }, 1000);
 
-        pr.getcurrentlist(function(data) {
-            $.each(data.result.list, function(index, item) {
-                var html = '<li><a class="'+index+'"><img src="'+this.cover+'" width="134" /><p>'+this.title+'</p><p style="color:#5b5b5b; font-size:0.75em;">'+this.year+'</p></a></li>';
-                $("#movies > ul").append(html);
-            });
-            console.log(data.result.list);
-        });
 
+        //Tabs handling
+        //Get the current tab
+        pr.getcurrenttab(function(data) {
+            $("#tabs a").removeClass("active");
+            //Set the actual tab active through the id
+            $('#'+data.result.tab).addClass("active");
+            //Call the function and pass the current tab to retrieve the right items list
+            getlist(data.result.tab);
+        });
     });
 
-    $(document).on("click", "#movies > ul > li > a", function() {
+
+
+    $(document).on("click", ".open-item", function() {
         var toSelect = $(this).attr('class');
         pr.setselection(toSelect);
         pr.enter();
     });
+
+    $("#movies").click(function() {
+        $("#tabs a").removeClass("active");
+        pr.movieslist();
+        $("#tabs > #movies").addClass("active");
+        getlist("movies");
+    });
+    $("#shows").click(function() {
+        $("#tabs a").removeClass("active");
+        pr.showslist();
+        $("#tabs > #shows").addClass("active");
+        getlist("shows");
+    });
+    $("#anime").click(function() {
+        $("#tabs a").removeClass("active");
+        pr.animelist();
+        $("#tabs > #anime").addClass("active");
+        getlist();
+    });
+
 
 
 });
